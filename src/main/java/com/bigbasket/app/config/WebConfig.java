2bacd2c1269b1086b9da11b0b7a6dd5f0127958b
Mohd. Asfaq-E-Azam Rifat <mohd.asfaqeazam@gmail.com>
2024-01-21 01:53:33 +0600

    ignore warning

--- src/main/java/com/bigbasket/app/config/WebConfig.java
+++ src/main/java/com/bigbasket/app/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+            @SuppressWarnings("NullableProblems")
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
